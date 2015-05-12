mildred/celutz

commit ffc661644b1642412ef076ac28003d0aa9764c24

Redirect to the panorama view after upload

panorama/views.py
@@ -26,10 +26,10 @@ class PanoramaView(DetailView):
 
 class PanoramaGenTiles(RedirectView):
     permanent = False
-    pattern_name = "panorama:list"
+    pattern_name = "panorama:view_pano"
 
     def get_redirect_url(self, *args, **kwargs):
-        pano = get_object_or_404(Panorama, pk=kwargs['id'])
+        pano = get_object_or_404(Panorama, pk=kwargs['pk'])
         pano.generate_tiles()
         return super(PanoramaGenTiles, self).get_redirect_url(*args, **kwargs)
 

panorama/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     url(r'^$', PanoramaList.as_view(), name="list"),
     url(r'^pano/new/$', PanoramaUpload.as_view(), name="new"),
     url(r'^pano/view/(?P<pk>\d+)/$', PanoramaView.as_view(), name="view_pano"),
-    url(r'^pano/gen_tiles/(?P<id>\d+)/$', PanoramaGenTiles.as_view(), name="gen_tiles"),
+    url(r'^pano/gen_tiles/(?P<pk>\d+)/$', PanoramaGenTiles.as_view(), name="gen_tiles"),
 )

commit 5887d4586826e8ff3cea8affa3be250193badaa4

Fix redirection bug introduced in ffc66164

panorama/views.py
@@ -16,7 +16,7 @@ class PanoramaUpload(CreateView):
     template_name = "panorama/new.html"
 
     def get_success_url(self):
-        return reverse_lazy("panorama:gen_tiles", kwargs={"id": self.object.id})
+        return reverse_lazy("panorama:gen_tiles", kwargs={"pk": self.object.id})
 
 class PanoramaView(DetailView):
     model = Panorama