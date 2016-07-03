1b780f138382f65ab4ac2e06874dfdfa3b946dc9
Ismael Benslimane <ismael@Charm>
2016-07-03 19:26:08 +0200

    Improved panorama style

--- panorama/static/panorama/js/pano.js
+++ panorama/static/panorama/js/pano.js
@@ -938,7 +938,7 @@ function clean_canvas_events(e) {
 }
 
 canvas_set_size = function() {
-    canvas.style.border = border_width+"px solid red";
+    canvas.style.border = border_width+"px solid black";
     canvas.width = window.innerWidth-2*border_width;
     canvas.height = window.innerHeight-2*border_width;
     canvas_pos.x = canvas.offsetLeft+border_width;
@@ -1091,3 +1091,79 @@ function load_pano() {
 	adding.addEventListener('mouseout', paramOut, false);
     }
 };
+	function toRad(n) {
+	  return n * Math.PI / 180;
+	}
+	function toDeg(n) {
+	  return n * 180 / Math.PI;
+	}
+	function destVincenty(lat1, lon1, brng, dist) {
+	  /* JavaScript function to calculate the destination point given start point
+	   * latitude / longitude (numeric degrees), bearing (numeric degrees) and
+	   * distance (in m).
+	   * Original scripts by Chris Veness
+	   * Taken from http://movable-type.co.uk/scripts/latlong-vincenty-direct.html
+	   * and optimized / cleaned up by Mathias Bynens <http://mathiasbynens.be/>
+	   *
+	   * Based on the Vincenty direct formula by T. Vincenty, “Direct and Inverse
+	   * Solutions of Geodesics on the Ellipsoid with application of nested
+	   * equations”, Survey Review, vol XXII no 176, 1975
+	   * <http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf>
+	   */
+	  var a = 6378137,
+	      b = 6356752.3142,
+	      f = 1 / 298.257223563, // WGS-84 ellipsiod
+	      s = dist,
+	      alpha1 = toRad(brng),
+	      sinAlpha1 = Math.sin(alpha1),
+	      cosAlpha1 = Math.cos(alpha1),
+	      tanU1 = (1 - f) * Math.tan(toRad(lat1)),
+	      cosU1 = 1 / Math.sqrt((1 + tanU1 * tanU1)), sinU1 = tanU1 * cosU1,
+	      sigma1 = Math.atan2(tanU1, cosAlpha1),
+	      sinAlpha = cosU1 * sinAlpha1,
+	      cosSqAlpha = 1 - sinAlpha * sinAlpha,
+	      uSq = cosSqAlpha * (a * a - b * b) / (b * b),
+	      A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq))),
+	      B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq))),
+	      sigma = s / (b * A),
+	      sigmaP = 2 * Math.PI;
+	  while (Math.abs(sigma - sigmaP) > 1e-12) {
+	    var cos2SigmaM = Math.cos(2 * sigma1 + sigma),
+		sinSigma = Math.sin(sigma),
+		cosSigma = Math.cos(sigma),
+		deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 * (cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM) - B / 6 * cos2SigmaM * (-3 + 4 * sinSigma * sinSigma) * (-3 + 4 * cos2SigmaM * cos2SigmaM)));
+	    sigmaP = sigma;
+	    sigma = s / (b * A) + deltaSigma;
+	  };
+	  var tmp = sinU1 * sinSigma - cosU1 * cosSigma * cosAlpha1,
+	      lat2 = Math.atan2(sinU1 * cosSigma + cosU1 * sinSigma * cosAlpha1, (1 - f) * Math.sqrt(sinAlpha * sinAlpha + tmp * tmp)),
+	      lambda = Math.atan2(sinSigma * sinAlpha1, cosU1 * cosSigma - sinU1 * sinSigma * cosAlpha1),
+	      C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha)),
+	      Lo = lambda - (1 - C) * f * sinAlpha * (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * cos2SigmaM * cos2SigmaM))),
+	      revAz = Math.atan2(sinAlpha, -tmp); // final bearing	
+
+	  return {lat: toDeg(lat2), lng: lon1 + toDeg(Lo)};
+	};
+	function getCone(lat, lng, bearing, angle, distance){
+	  /* Returns a polygon to be drawn to the map to show the current visual field
+	   */
+	  var conepoints = [];
+	  // by default, points are drawn every 5°, but if the angle to draw is
+	  // smaller than 5°, we draw no intermediary points
+	  var delta = 5;
+	  if (angle < 5){delta=angle};
+
+	  conepoints.push ([lat,lng]);
+
+	  for (i=0; i<=angle; i+=delta){
+	    conepoints.push([destVincenty(lat, lng, bearing-(angle/2.0)+i, distance).lat,
+		destVincenty(lat, lng, bearing-(angle/2.0)+i, distance).lng]);
+	  }
+
+	  var p = L.polygon(conepoints, {
+	      color: 'grey',
+	      fillColor: 'grey',
+	      fillOpacity: 0.5
+	  });
+	  return p;
+	};
