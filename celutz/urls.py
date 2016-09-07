0de5a72a7e25ea624d0481e97fc3ad9924343c09
Baptiste Jonglez <git@bitsofnetworks.org>
2016-09-07 22:38:51 +0200

    Revert "Activate localized URL prefix"
    This reverts commit a52bf9db5a5d86f26a677cd14a4d77b2de16eb54.
    
    Apparently, this breaks all URLs in some cases (browser issue? language
    autodetection issue in Django? Python2/3 compatibility issue?).  Revert
    until we can sort this out.

--- celutz/urls.py
+++ celutz/urls.py
@@ -1,19 +1,13 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'celutz.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/v1/', include('api.urls')),
-]
-
-urlpatterns += i18n_patterns(
     url(r'^', include('panorama.urls', namespace="panorama")),
-)
-
+    url(r'^api/v1/', include('api.urls')),
 # In debug mode, serve tiles
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
