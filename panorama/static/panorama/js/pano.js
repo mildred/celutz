eea82a6916f16e3fdd500b563d05c3abc1bb10da
Baptiste Jonglez <git@bitsofnetworks.org>
2016-09-18 11:13:39 +0200

    Use HTTPS to load tiles from Openstreetmap

--- panorama/static/panorama/js/pano.js
+++ panorama/static/panorama/js/pano.js
@@ -1234,8 +1234,8 @@ function load_map(){
     map = L.map('mapid').setView([panorama_lat, panorama_lng], 13);
 
     // create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});		
 	map.addLayer(osm);
         map.addLayer( markerClusters );
