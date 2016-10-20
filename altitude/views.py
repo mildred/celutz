07b261d2c5cd19ed8b3ef47c9b18d5b5f185c52e
Baptiste Jonglez <git@bitsofnetworks.org>
2016-10-20 17:11:01 +0200

    altitude: Fix commit 455b941eb1 for python2 and add more validation
    Use "requests" to avoid compatibility issues with urllib/urllib2 between
    python2 and python3.
    
    Also add more validation about what api.geonames.org might return.

--- altitude/views.py
+++ altitude/views.py
@@ -1,11 +1,23 @@
-import urllib
+import requests
+import logging
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseServerError
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def geonames_altitude(request, lat, lon):
     lat = float(lat)
     lon = float(lon)
     url = settings.GEONAMES_ASTERGDEM.format(lat=lat, lon=lon)
-    return HttpResponse(urllib.request.urlopen(url))
+    r = requests.get(url)
+    if r.status_code != 200:
+        return HttpResponseServerError()
+    # The API sometimes returns an error but still sends a 200 code,
+    # so we validate the answer just to make sure...
+    try:
+        return HttpResponse(float(r.text))
+    except ValueError:
+        logger.warning("api.geonames.org error: {}".format(r.text))
+        return HttpResponseServerError()
